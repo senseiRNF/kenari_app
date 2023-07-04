706054322b87186661a4f30c2a00bfb15e500fc0
Razy Novian <razy.firdana@intidata.net>
2023-07-04 11:35:52 +0700

    update API options

--- lib/services/api/api_options.dart
+++ lib/services/api/api_options.dart
@@ -33,32 +33,3 @@ class APIOptions {
     return dio;
   }
 }
-
-class TempAPIOptions {
-  static Future<Dio> init() async {
-    Dio dio = Dio();
-
-    dio.options = BaseOptions(
-      baseUrl: 'http://10.10.50.17:8082/api',
-      headers: {
-        'Accept': 'application/json',
-      },
-      connectTimeout: const Duration(seconds: 10),
-      receiveTimeout: const Duration(seconds: 60),
-    );
-
-    IOHttpClientAdapter clientAdapter = IOHttpClientAdapter(
-      createHttpClient: () {
-        HttpClient client = HttpClient();
-
-        client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
-
-        return client;
-      },
-    );
-
-    dio.httpClientAdapter = clientAdapter;
-
-    return dio;
-  }
-}
